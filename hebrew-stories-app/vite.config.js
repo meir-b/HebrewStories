f66d2e1d95b0f1c27e0cf88693e37e29a0c5ddb9
meir_b <mebloya@microsoft.com>
2025-04-16 18:09:07 +0300

    Build

--- hebrew-stories-app/vite.config.js
+++ hebrew-stories-app/vite.config.js
@@ -3,5 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "https://meir-b.github.io/HebrewStories/hebrew-stories-app/dist/",
   plugins: [vue()],
 })
